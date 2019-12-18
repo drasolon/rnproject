a8432f4c8d385965df4e6b3abf85e546fc8fe0d0
David Rasolondramanitra <david.rasolond@gmail.com>
2019-12-18 11:05:13 +0100

    added data

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,40 +1,51 @@
 import React, {Component} from 'react';
-import {SafeAreaView, StatusBar, FlatList} from 'react-native';
+import {
+  SafeAreaView,
+  StatusBar,
+  ActivityIndicator,
+  View,
+  FlatList,
+} from 'react-native';
 import Post from './Post';
 import TopBar from './TopBar';
 
-const DATA = [
-  {
-    id: '1',
-    title: 'title 1',
-    content: 'Post 1',
-  },
-  {
-    id: '2',
-    title: 'title 2',
-    content: 'Post 2',
-  },
-  {
-    id: '3',
-    title: 'title 3',
-    content: 'Post 3',
-  },
-];
-
 export default class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoading: true};
+  }
+
+  componentDidMount() {
+    return fetch('http://reddit.it.ws312.net:3000/posts?fromApp=1')
+      .then(response => response.json())
+      .then(results => {
+        console.log(results);
+        this.setState({
+          isLoading: false,
+          data: results,
+        });
+      });
+  }
+
   _displayPost = id => {
-    console.log('Display post with id ' + id);
     this.props.navigation.navigate('PostDetails', {id: id});
   };
 
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
     return (
       <>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView>
           <TopBar></TopBar>
           <FlatList
-            data={DATA}
+            data={this.state.data.result}
             renderItem={({item}) => (
               <Post post={item} displayPost={this._displayPost} />
             )}
